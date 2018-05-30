74491717047ca8a480dafaf89382b5fe80e9e5d5
novoa97 <novoa199@gmail.com>
2018-05-30 15:44:34 +0200

    Paso1

---
+++ src/app/comment/comment.ts
@@ -0,0 +1,11 @@
+export interface comment{
+  id: number,
+  text: string,
+  votes: number,
+  parent_id: number,
+  created_at: string,
+  updated_at: string,
+  post_id: string,
+  user_id: string,
+  tipus: string
+}
