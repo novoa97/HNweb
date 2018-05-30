009451aa6659258f79a73312ed6b6ddf88e38147
novoa97 <novoa199@gmail.com>
2018-05-30 13:19:45 +0200

    Intento deploy 2

--- server.js
+++ server.js
@@ -14,3 +14,6 @@ res.sendFile(path.join(__dirname+'/dist/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+
+console.log('Console listening!');
