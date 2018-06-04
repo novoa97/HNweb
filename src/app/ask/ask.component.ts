f84ab2ec4ceef95235c4562032f584441521bd76
mabaro3009 <mabaro3009@hotmail.com>
2018-06-04 06:48:33 +0200

    vote ask y newest

--- src/app/ask/ask.component.ts
+++ src/app/ask/ask.component.ts
@@ -6,6 +6,8 @@ import { post } from '../post/post'
 import { UserService } from '../user/user.service'
 import { user } from '../user/user'
 
+import { Router }  from "@angular/router";
+
 @Component({
   selector: 'app-ask',
   templateUrl: './ask.component.html',
@@ -16,7 +18,7 @@ export class AskComponent implements OnInit {
   asks: post[]
   idsesion: number = null;
   
-  constructor( private PostService: PostService, private UserService: UserService) { }
+  constructor( private PostService: PostService, private UserService: UserService, private router: Router) { }
 
   ngOnInit() {
     this.getAsk()
@@ -30,9 +32,27 @@ export class AskComponent implements OnInit {
       this.asks = posts
       for ( let p of this.asks ){
         this.UserService.getuser(p.user_id).subscribe(user => p.user_name = user.name)
+		this.PostService.getVoted(p.id).subscribe( (response) =>{
+            p.voted = response
+            console.log(response)
+          })
       }
     })
 
+  }
+  
+  vote(i): void{
+      if (localStorage.getItem("token")){
+        this.asks[i].voted = true;
+        this.PostService.vote(this.asks[i].id).subscribe( response => console.log(response))
+        this.asks[i].upvotes_count = this.asks[i].upvotes_count + 1
+      }
+      else this.router.navigate(['/login']);
+  }
+  unvote(i){
+      this.asks[i].voted = false
+      this.PostService.unvote(this.asks[i].id).subscribe( response => console.log(response))
+      this.asks[i].upvotes_count = this.asks[i].upvotes_count - 1
   }
   
   deletePost(index) {
